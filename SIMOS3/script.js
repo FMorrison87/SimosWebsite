15c6876faccc3b2732b12074327f94e12f82b885
Frances Morrison <Frances.Morrison87@gmail.com>
2019-04-01 07:46:48 -0400

    Still working on Simos Website.

--- SIMOS3/script.js
+++ SIMOS3/script.js
@@ -1,18 +1,18 @@
 const navbar = document.querySelector(".navbar");
 
 function darkenNav() {
-  if (window.scrollY == 0) {
+  if (window.scrollY <= 0) {
     navbar.classList.remove("bg-light");
-    navbar.classList.remove("navbar-light");
-    navbar.classList.add("navbar-dark");
+    navbar.classList.remove("shadow-sm");
   } else {
     navbar.classList.add("bg-light");
-    navbar.classList.remove("navbar-dark");
     navbar.classList.add("navbar-light");
+    navbar.classList.add("shadow-sm");
   }
 }
 
-// document.addEventListener("scroll", darkenNav);
+document.addEventListener("scroll", darkenNav);
+document.addEventListener("DOMContentLoaded", darkenNav);
 
 const navItems = document.querySelectorAll(".navbar-nav li a");
 
@@ -26,27 +26,4 @@ navItems.forEach(item => item.addEventListener("click", switchActive));
 const logoShow = document.querySelector(".simosLogo");
 const navbarItems = document.querySelector("#navbarNav ul");
 const navbarButton = document.querySelector("#nav-contact-button");
-const logoHeader = document.querySelector(".logo-header").offsetHeight;
 const hero = document.querySelector(".hero");
-
-function navbarChanges() {
-  if (window.scrollY <= logoHeader) {
-    logoShow.setAttribute("style", "display: none;");
-    navbarButton.setAttribute("style", "display: none;");
-    navbarItems.classList.remove("ml-auto");
-    navbarItems.classList.add("mx-auto");
-    navbar.classList.add("sticky-top");
-    navbar.classList.remove("fixed-top");
-    hero.classList.remove("sticky-offset");
-  } else {
-    logoShow.setAttribute("style", "display: block;");
-    navbarButton.setAttribute("style", "display: block;");
-    navbarItems.classList.add("ml-auto");
-    navbarItems.classList.remove("mx-auto");
-    navbar.classList.remove("sticky-top");
-    navbar.classList.add("fixed-top");
-    hero.classList.add("sticky-offset");
-  }
-}
-
-// document.addEventListener("scroll", navbarChanges);
